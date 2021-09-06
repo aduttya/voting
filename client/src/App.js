29a76a2dd704de84c281bcb3689fbda94a04ad64
aduttya <anjayyadaav379@gmail.com>
2021-09-06 13:30:10 +0530

    app.js comment added

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 import { func } from "prop-types";
 
+// create a react app
 class App extends Component {
 
+  // initializing the react states 
   constructor(props){
     super(props);
     this.state = {
@@ -22,7 +24,8 @@ class App extends Component {
 
   }
 
-  
+  // it is the first function that will automatically be called
+
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
@@ -31,16 +34,20 @@ class App extends Component {
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
       console.log(accounts)
-      // Get the contract instance.
+
+      // Get the network instance 
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = await Voting.networks[networkId];
+
+      // getting contract instance
       const instance = new web3.eth.Contract(
         Voting.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
       
-        console.log('the val of index is ',this.state.index)
+       // console.log('the val of index is ',this.state.index)
+      //  updating the states
       this.setState({ web3:web3, accounts, contract: instance ,address:deployedNetwork.address});
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -51,12 +58,16 @@ class App extends Component {
     }
   };
 
-
+// function to add candidates
 Addcandidate = async()=>{
 
   const {addcandidate,web3,contract,accounts} = this.state;
+  // if user types null string
+
     if(addcandidate != ""){
     console.log('add candidate function is called and name of the candidate is',addcandidate)
+
+    // web3.js method to call the deployed function(contract is the object to access functions)
    await contract.methods.Addcandidate(addcandidate).send({
      from : accounts[0],
      gas : 800000
@@ -69,11 +80,17 @@ Addcandidate = async()=>{
 
 }
 
+// getting total added candidates
 totalCandidate = async()=>{
   console.log('total candidate function is called')
+
+  // getting contract instance from react state 
   const {contract} = this.state
+
+  // calling totoalCandidates method
   let len = await contract.methods.totalCandidates().call()
 
+  // print total candidates with their votes
   for(let i = 0; i < len; ++i){
     await contract.methods.candidates(i).call().then(function(results){
       console.log('name of the candidate is ',results[0],"and he got",results[1].toString()," votes")
@@ -85,12 +102,13 @@ totalCandidate = async()=>{
   
 }
 
+// function to add voters
 Addingvoters = async ()=>{
   const{contract,accounts,addvoter,web3} = this.state
   console.log('adding voters function is called and voter address is',addvoter)
  
-
-  await contract.methods.Addvoter(addvoter).send({
+    // web3.js method to call the deployed function
+    await contract.methods.Addvoter(addvoter).send({
     from:accounts[0],
     gas : 800000
   }).then( await contract.methods.Voters(addvoter).call().then(function(results){
@@ -99,9 +117,15 @@ Addingvoters = async ()=>{
 
 
 }
+
+// function to use vote function
 voting = async ()=>{
+
+  // getting react instances
   const{contract,accounts,index,address} = this.state
   console.log('voting function is called and the value of index is',index)
+
+  // calling vote function
   await contract.methods.vote(index).send({
     from:accounts[0],
     to:address,
@@ -110,7 +134,7 @@ voting = async ()=>{
 
 
 }
-
+// checks the state of a voter that he is voted or not
 checkvotedPeople = async ()=>{
   const{contract,accounts,addvoter} = this.state
   console.log('the account address is ',this.addvoter)
@@ -122,6 +146,8 @@ checkvotedPeople = async ()=>{
 
 
   render() {
+
+    // it loads first to check web3 provider(metamask in this case)
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
